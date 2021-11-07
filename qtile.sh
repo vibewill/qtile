754892f3bca87116628dd5368e3c7b8f88a8b2fe
vibewill <vibewill@gmail.com>
2021-11-07 11:00:28 -0300

    programas

---
+++ qtile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install xorg 
+sudo apt install rxvt-unicode pulseaudio alsa-utils compton feh build-essetial python3-pip 
+sudo apt install fonts-font-awesome fonts-noto vim firefox-esr rofi git
+sudo apt install python3-cffi python3-xcffib python3-cairocffi
+sudo pip3 install psutil
